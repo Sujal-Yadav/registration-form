4efb13cdff601a159830a3480f3da25699ae2480
Sujal-Yadav <sujjalyadav@gmail.com>
2023-12-08 13:09:01 +0530

    UI changes and overall optimization

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const port = 4000;
+const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1:27017/bharat_intern');
 
@@ -25,10 +25,9 @@ app.get('/login', (req, res) => {
 
 app.post('/registration', async (req, res) => {
     try {
-        const existingUser = await user.findOne({ email: req.body.email });
-        console.log(req.body);
+        const existingUser = await user.find({ email: req.body.email });
 
-        if (existingUser) {
+        if (existingUser > 0) {
             // return res.status(400).send('User already registered');
             return res.status(400).sendFile("pages/login.html", {root: __dirname});
         }
@@ -45,13 +44,25 @@ app.post('/registration', async (req, res) => {
     }
 });
 
+app.get('/registration', async (req, res) => {
+    res.sendFile('pages/signup.html', {root: __dirname});
+})
+app.get('/validation', async (req, res) => {
+    res.sendFile('pages/login.html', {root: __dirname});
+})
+
 app.post('/validation', async (req, res) => {
     try {
         const existingUser = await user.findOne({ email: req.body.email });
-        console.log(req.body);
-
+        console.log(existingUser);
         if (existingUser) {
-            return res.status(400).sendFile("pages/register.html", {root: __dirname});
+            if(existingUser.email == req.body.email && 
+                existingUser.password === req.body.password) {
+                return res.status(200).sendFile("pages/register.html", {root: __dirname});
+            }
+            else{
+                return res.status(401).send('Incorrect Password');
+            }
         }
         else {
             return res.status(200).sendFile("pages/signup.html", {root: __dirname});
